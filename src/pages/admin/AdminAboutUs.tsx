c1882224d6c9cb77d267e738c2e216a2dd0c444b
shresthanikhil16 <shresthanikhil16@gmail.com>
2023-12-27 17:36:09 +0545

    Admin about us

---
+++ src/pages/admin/AdminAboutUs.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './AdminAboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <div className='Image'>
+      <div className="about-us-container">
+        <img src="/images/template1.png" alt="Description of the image" className="template-image" />
+      
+        <div className={"about-us-body"}>
+                <h1>About Us</h1>
+                <p>If you are looking for a website that offers the very best online shopping in Nepal, choose LUGA HUB. We are a trusted and easy to use shopping site that offers a surprising, and exciting, range of top quality products, at affordable prices.
+                    LUGA HUB is easy to use and we provide a level of customer service that we believe is second to none. Our aim is to make LUGA HUB people’s first choice when they are looking for top quality and affordable online shopping in Nepal.</p>
+                <h2>LUGA HUB-your best choice for online shopping in Nepal</h2>
+                <p>We have designed each and every aspect of our service with you, our customer, in mind:</p>
+                <h3>Products:</h3>
+                <p>We offer the widest range of products, in a variety of categories including electronics, fashion, children's wear, home ware, furnishings, fitness, outdoors and much, more. Whether you are buying for yourself or wish to send a gift to someone in Nepal, we have a vast selection of suitable items available (we also offer a gift wrapping service, on request);</p>
+                <h3>Customer Satisfaction:</h3>
+                <p>As one of the largest websites offering online shopping in Nepal, we combine an extensive range of products with a commitment to 100% customer satisfaction. So,  if you have a problem with any aspect of the service you receive from us, simply get in touch and you can be confident that we will do everything we can to help;</p>
+                <h3>Delivery:</h3>
+                <p>We will provide you with a fast, trustworthy and reliable delivery service regardless of where you live in Nepal. Our charges are reasonable and our order tracking service allows you to see when your order will be delivered. In addition, we offer free delivery to our customers in Kathmandu. If you would prefer to collect your purchases in person from our store, simply contact us now on 9860929065 to find out more about our free Scheduled Collection Service;</p>
+                <h3>Website:</h3>
+                <p>Our website makes it easy for you to find the products you require and can be accessed using a PC, laptop, tablet or smartphone, 24 hours a day, 365 days a year;</p>
+                <h3>Pricing:</h3>
+                <p>LUGA HUB makes online shopping in Nepal the ideal way to find quality products at low prices. We source quality products carefully, buy in bulk and pass on our savings to you, in the form of lower prices and great discounts;</p>
+                <h3>Returns:</h3>
+                <p>Our 'refund or replace' returns policy allows you to buy in complete confidence;</p>
+                <h3>Payments:</h3>
+                <p>All website payments are security encrypted providing our customers with a safe, secure and trusted online payments system. We also offer cash on delivery options;</p>
+                <h3>Discounts:</h3>
+                <p>With our daily and  weekly discount deals, you have the opportunity to buy quality products at low, low prices;LUGA HUB offers high quality, great prices and a level of customer service that is second to none. We don't just make online shopping in Nepal affordable, we make it easy and enjoyable, too.</p>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
